e4c9da0618b907cf056ffe684564371cbe8b5d18
aissa-dev1 <aissabedr74@gmail.com>
2024-09-04 21:41:53 -0700

    feat: optimise for SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head />
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="description" content="Next.js Dashboard website." />
+        <meta property="og:title" content="Dashboard" />
+        <meta property="og:description" content="Next.js Dashboard website." />
+        <meta
+          property="og:url"
+          content="https://aissa-dev1-next-dashboard.netlify.app"
+        />
+        <meta property="og:site_name" content="Dashboard" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
